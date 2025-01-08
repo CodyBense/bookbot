6811f59fd8d9f06833d585b585ebb974816018a6
CodyBense <codybense@gmail.com>
2025-01-08 11:22:35 -0500

    finished

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def main():
     path = "./books/frankenstein.txt"
-    text = get_book_text(path)
-    num_words = get_word_count(text)
-    chars_dict = get_char_count(text)
-    print(f"{num_words} words were found in frankenstein")
-    print(chars_dict)
+    report(path)
 
 def get_book_text(path):
     with open("./books/frankenstein.txt") as f:
@@ -18,9 +14,33 @@ def get_char_count(text):
     lower = text.lower()
     chars = {}
     for char in lower:
-        if char not in chars:
-            chars[char] = 1
-        else:
-            chars[char] += 1
+        if char.isalpha():
+            if char not in chars:
+                chars[char] = 1
+            else:
+                chars[char] += 1
     return chars
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_chars(dict):
+    chars_list = []
+    for item in dict:
+        chars_list.append({"char": item, "count": dict[item]})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
+
+def report(path):
+    text = get_book_text(path)
+    num_words = get_word_count(text)
+    chars_dict = get_char_count(text)
+    chars_list = sort_chars(chars_dict)
+    # print(sort_chars(chars_dict))
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words were found in frankenstein\n")
+    for char in chars_list:
+        print(f"The '{char["char"]}' character was found {char["count"]} times")
+    print("--- End report ---")
+
 main()
